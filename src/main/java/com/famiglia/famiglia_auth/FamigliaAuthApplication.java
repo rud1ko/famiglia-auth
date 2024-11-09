0c0a2e03b7e7c0e02e4f7085c1341def126532dd
Rudiko777 <chakryan_rudik@mail.ru>
2024-11-09 12:54:45 +0300

    first commit

---
+++ src/main/java/com/famiglia/famiglia_auth/FamigliaAuthApplication.java
@@ -0,0 +1,13 @@
+package com.famiglia.famiglia_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamigliaAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamigliaAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/famiglia/famiglia_auth/FamigliaAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.famiglia.famiglia_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamigliaAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
